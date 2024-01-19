899f04b2bb444060058eee9388a66c886b920221
rparedestavaradev <roggerparedes23@gmail.com>
2024-01-19 17:38:09 -0300

    First design in component login and It was added logo without background to design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './features/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './core/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     HttpClientModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 import { LoginServiceAdapter } from 'src/app/adapters/login-service.adapter';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,12 @@ export class LoginComponent implements OnInit {
 
   email: string = "";
   password: string = "";
+  environment: string = environment.descriptionEnvironment;
+  version: string = VERSION.full;
+  forgot_password: string = 'Forgot password';
+  description_recover_password: string = "Recover password";
+  register_message: string = "If you don’t have an accounte, you can register at";
+  description_to_register: string = "Register User";
 
   constructor() { }
 
