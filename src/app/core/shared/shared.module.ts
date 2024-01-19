58871e8fe7db959569a54c843ad6b0201fd68487
rparedestavaradev <roggerparedes23@gmail.com>
2024-01-19 17:37:02 -0300

    custom-input and custom-button were added to shared module

--- src/app/core/shared/shared.module.ts
+++ src/app/core/shared/shared.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { CustomButtonComponent } from './custom-button/custom-button.component';
+import { CustomInputComponent } from './custom-input/custom-input.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CustomButtonComponent,
+    CustomInputComponent
   ],
   imports: [
     CommonModule
@@ -14,6 +18,8 @@ import { HeaderComponent } from './header/header.component';
   exports: [
     FooterComponent,
     HeaderComponent,
+    CustomButtonComponent,
+    CustomInputComponent
   ]
 })
 export class SharedModule { }
