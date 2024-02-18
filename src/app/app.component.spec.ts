a843b7d4237c03b8253775619eac91776c89324f
rparedestavaradev <roggerparedes23@gmail.com>
2024-02-18 02:17:21 -0300

    Clean code with test gpp v2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('login-front-rp-music');
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('login-front-rp-music app is running!');
-  // });
 });
